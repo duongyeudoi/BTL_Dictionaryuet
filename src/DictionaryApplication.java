26c154f2cde3777b8345cbe60a6e66f99913c11b
Nguyễn Quang Đại Dương <19020265@vnu.edu.vn>
2020-10-14 00:26:21 +0700

    fix UI

--- src/DictionaryApplication.java
+++ src/DictionaryApplication.java
@@ -12,7 +12,7 @@ public class DictionaryApplication extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         //    primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
     }
 
